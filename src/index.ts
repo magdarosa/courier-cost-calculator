070cd5578a8e7b02c289ef387b47f0cb49b66a5d
magdarosa <magda.sharon@gmail.com>
2025-08-11 21:32:52 +1200

    feat(step1): add public exports to index file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 // Courier Cost Calculator Library - Public exports
 export * from './types';
+export { Parcel } from './models/parcel';
+export { Order } from './models/order';
+export { ShippingCalculator } from './services/shippingCalculator';
+export { ParcelSizingService } from './services/parcelSizingService';
